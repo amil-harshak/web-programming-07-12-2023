cdfc01ac82e30de917ba7f37fea35e65c12523f0
amil-harshak <147579641+amil-harshak@users.noreply.github.com>
2023-12-07 11:42:45 +0530

    Add files via upload

---
+++ check.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<form action="evenodd.php" method="get">
+<p>Enter a number to check whether its even or odd</p>
+Number : <input type="text" name="txtnum"><br>
+<input type="submit">
+</form>
+</body>
+</html>
+
---
+++ evenodd.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<?php $x=$_GET["txtnum"];
+if($x%2===0)
+{
+echo "the number is even ";
+}
+else{
+echo "the number is odd";
+}
+?>
+</body>
+</html>
